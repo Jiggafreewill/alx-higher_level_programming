f35f9f0cc5730788d4e1556703ec164f80979974
Jiggafreewill <jiggafreewill@gmail.com>
2023-12-07 18:25:52 -0800

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/13-main.c
+++ 0x03-python-data_structures/13-main.c
@@ -1,7 +1,7 @@
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+
 /**
  * main - check the code for
  *
@@ -10,6 +10,7 @@
 int main(void)
 {
     listint_t *head;
+
     head = NULL;
     add_nodeint_end(&head, 1);
     add_nodeint_end(&head, 17);
@@ -22,10 +23,13 @@ int main(void)
     add_nodeint_end(&head, 17);
     add_nodeint_end(&head, 1);
     print_listint(head);
+
     if (is_palindrome(&head) == 1)
         printf("Linked list is a palindrome\n");
     else
         printf("Linked list is not a palindrome\n");
+
     free_listint(head);
+
     return (0);
 }
